1d1e199eb2fbb1f506490f6ff64b961fac66223b
Konrad <karmatys888@gmail.com>
2024-01-16 19:21:32 +0100

    Added EmailTextField

--- FrontEnd/src/sections/register/register-view.jsx
+++ FrontEnd/src/sections/register/register-view.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -12,6 +12,7 @@ import { alpha, useTheme } from '@mui/material/styles';
 import { useRouter } from 'src/routes/hooks';
 
 import { bgGradient } from 'src/theme/css';
+import EmailTextField from 'src/layouts/dashboard/common/email-text-field';
 import PasswordTextField from 'src/layouts/dashboard/common/password-text-field';
 
 import Logo from 'src/components/logo';
@@ -21,49 +22,57 @@ export default function RegisterView() {
   const theme = useTheme();
 
   const router = useRouter();
-  const [registerData, setRegisterData] = useState({ firstName: '', lastName: '',
-                                                        email: '', password: '' ,confirmPassword: ''});
+  const [nameData, setNameData] = useState({ firstName: '', lastName: '' });
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-  const handleInputChange = event => {
-      const { name, value } = event.target;
-      setRegisterData(prevData => ({
-          ...prevData,
-          [name]: value
-      }));
+  const handleNameChange = (event) => {
+    const { name, value } = event.target;
+    setNameData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const handleClick = async () => {
-      console.log(registerData);
-      try {
-          const response = await fetch('http://localhost:3000/users/register', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(registerData)
-          });
-          if(response.ok){
-              const responseData = await response.json();
-              router.push('/login');
-          }
-      } catch (error) {
-          console.error('Network error:', error.message);
+    try {
+      const response = await fetch('http://localhost:3000/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...nameData, email, password, confirmPassword }),
+      });
+      if (response.ok) {
+        await response.json();
+
+        router.push('/login');
       }
+    } catch (error) {
+      console.error('Network error:', error.message);
+    }
   };
 
   const renderForm = (
     <>
       <Stack spacing={3}>
-        <TextField name="firstName" label="First name" onChange={handleInputChange}/>
-        
-        <TextField name="lastName" label="Last name" onChange={handleInputChange} />
+        <TextField name="firstName" label="First name" onChange={handleNameChange} />
 
-        <TextField name="email" label="Email address" onChange={handleInputChange}/>
+        <TextField name="lastName" label="Last name" onChange={handleNameChange} />
 
-        <TextField name="password" label="Password" onChange={handleInputChange} />
+        <EmailTextField email={email} setEmail={setEmail} />
 
-        <TextField name="confirmPassword" label="Confirm password" onChange={handleInputChange} />
+        <PasswordTextField name="password" label="Password" setState={setPassword} />
+
+        <PasswordTextField
+          name="confirmPassword"
+          label="Confirm password"
+          setState={setConfirmPassword}
+          error={password !== confirmPassword}
+          helperText={password !== confirmPassword ? 'Passwords do not match' : ''}
+        />
       </Stack>
 
       <LoadingButton
@@ -109,8 +118,6 @@ export default function RegisterView() {
           <Typography variant="h4" sx={{ mb: 5 }}>
             Register to Minimal
           </Typography>
-
-      
 
           <Divider sx={{ my: 3 }} />
 
