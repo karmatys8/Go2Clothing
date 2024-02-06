ce41d3317f8df335e31ccd27e1f638b97e8d3f8e
Mateusz <zsuetam36@gmail.com>
2024-02-06 23:35:40 +0100

    modified and fiexd endpoint for adding new orders - need to check it before merging but must wait until work on another branch will be done

--- BackEnd/routes/customerRouter.js
+++ BackEnd/routes/customerRouter.js
@@ -48,10 +48,16 @@ router.get('/', decodeTokenMiddleware, async (req, res) => {
     }
 });
 
-router.post('/newOrder', async (req, res) => {
+router.post('/newOrder', decodeTokenMiddleware, async (req, res) => {
+    const userID = req.decodedToken.userID;
     try {
         const pool = await sql.connect(dbConfig);
         const { CustomerID, DeliveryDays, Freight, OrderDetails } = req.body;
+
+        if (userID !== CustomerID) {
+            console.log('Authentication error');
+            res.status(500).json({ success: false, error: 'Authentication error' });
+        }
 
         const result = await sql.query`
             DECLARE @NewOrderID INT;
@@ -68,8 +74,9 @@ router.post('/newOrder', async (req, res) => {
         const NewOrderID = result.recordset[0].NewOrderID;
 
         for (const detail of OrderDetails) {
+            const result = await sql.query`select dbo.GetProductDetailsID(${detail.ProductID},${detail.Color},${detail.Size}) AS Product_DetailsID`;
             await sql.query`
-                EXEC InsertOrderDetails ${NewOrderID},${detail.Product_DetailsID},${detail.Quantity},${detail.Discount}
+                EXEC InsertOrderDetails ${NewOrderID},${result.recordset[0].Product_DetailsID},${detail.Quantity}
             `;
         }
         await sql.close();
